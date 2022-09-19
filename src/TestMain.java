6bc5abebfdb68c9af39d895441c9b5626646a24c
sigmaOC <stephanedartinet@orange.fr>
2022-09-19 11:24:22 +0200

    Premier commit TestGitEclipse / affiche console "Hello"

---
+++ src/TestMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author steph
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
