1754097946d40b6ff937048b0e9bfeae3f5ea436
sigmaOC <stephanedartinet@orange.fr>
2022-09-19 11:59:18 +0200

    modif boucle

--- src/TestMain.java
+++ src/TestMain.java
@@ -12,7 +12,7 @@ public class TestMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 3; i++) {
 			System.out.println("Hello World");
 		}
 
